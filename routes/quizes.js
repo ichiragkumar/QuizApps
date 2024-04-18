775c55d956eafb5806ea50f713191056ad331f12
ichiragkumar <chiragroy3030@gmail.com>
2024-04-18 20:00:16 +0530

    Setting up quizes Routes

---
+++ routes/quizes.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const router = express.Router()
+
+
+
+
+// create quiz
+router.post("/quizzes/create ", (req, res)=>{
+
+
+
+
+})
+
+
+// get all the active quizes
+router.get("/quizzes/active", (req, res)=>{
+
+
+})
+
+
+// Get wuiz result
+router.get("/quizzes/:id/result", (req, res)=>{
+
+
+
+})
+
+
+// get all the quizes [Active or Inactive]
+router.get("/quizzes/all", (req, res)=>{
+
+
+})
