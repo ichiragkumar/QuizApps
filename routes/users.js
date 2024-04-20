38ac3224e8c5984748d2f928210aff3b5daeca06
ichiragkumar <chiragroy3030@gmail.com>
2024-04-21 03:02:41 +0530

    password hashed

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const express = require("express")
-
+const bcrypt = require('bcrypt');
 const router = express.Router()
 router.use(express.json())
 
@@ -12,18 +12,29 @@ router.post("/createUser", async (req, res)=>{
         const userDetails = req.body
         const parsedResult = createAccountSchema.safeParse(userDetails)
         if (parsedResult.success) {
-            try {
-                const user = await User.create(userDetails)
-                res.status(201).json({user,msg:"created succesfully"})
-            } catch (error) {
-                console.log(`Database Error ${error}`)
-                res.status(411).json({msg:"Database Error"})
-            }
-        }else {
-        const error = parsedResult.error;
-        console.log(error);
-        res.status(201).json({msg:"Invalid Input"})
-    }
+                const user = await User.findOne({username:userDetails.username})
+                if(!user){
+                    const saltRounds = 10
+                    
+                    bcrypt.hash(userDetails.password, saltRounds,async function(err, hash) {
+                        const dbUser = await User.create({
+                            username:userDetails.username,
+                            password:hash,
+                        })
+                        res.status(201).json({dbUser,msg:"created succesfully"})
+                    })
+                   
+                   
+                    
+                }else{
+                    res.status(201).json({msg:"Already Exist"})
+                }
+    
+            }else{
+                const error = parsedResult.error;
+                console.log(error);
+                res.status(201).json({msg:"Invalid Input"})
+        }
 })
 
 
